da90cd7067cff4beb091e539bd26de3fc7442a4f
Michael Mykhaylov <32168861+mikemykhaylov@users.noreply.github.com>
2025-04-03 10:52:50 -0700

    Disable billing cache

--- src/app/(protected)/api/ai/route.ts
+++ src/app/(protected)/api/ai/route.ts
@@ -1,11 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 import {
-    PipelineRunState,
+    // PipelineRunState,
     StartPipelineParams,
     gumloopService,
 } from '@/lib/services/gumloop';
-import { createClient } from '@/lib/supabase/supabaseServer';
+
+// import { createClient } from '@/lib/supabase/supabaseServer';
 
 export async function POST(request: NextRequest) {
     try {
@@ -51,41 +52,41 @@ export async function GET(request: NextRequest) {
 
         const status = await gumloopService.getPipelineRun({ runId });
 
-        if (status.state == PipelineRunState.DONE) {
-            console.log(
-                `Adding ${status.credit_cost} cost to the billing cache`,
-            );
+        // if (status.state == PipelineRunState.DONE) {
+        //     console.log(
+        //         `Adding ${status.credit_cost} cost to the billing cache`,
+        //     );
 
-            // increment the API usage counter
-            const supabase = await createClient();
-            const { data: billingRecord, error } = await supabase
-                .from('billing_cache')
-                .select('*')
-                .eq('organization_id', organizationId)
-                .single();
+        //     // increment the API usage counter
+        //     const supabase = await createClient();
+        //     const { data: billingRecord, error } = await supabase
+        //         .from('billing_cache')
+        //         .select('*')
+        //         .eq('organization_id', organizationId)
+        //         .single();
 
-            if (error) throw error;
+        //     if (error) throw error;
 
-            // @ts-expect-error The property exists
-            billingRecord.current_period_usage.api_calls += status.credit_cost;
-            if (!billingRecord.current_period_usage) {
-                throw new Error('No billing record found');
-            }
+        //     // @ts-expect-error The property exists
+        //     billingRecord.current_period_usage.api_calls += status.credit_cost;
+        //     if (!billingRecord.current_period_usage) {
+        //         throw new Error('No billing record found');
+        //     }
 
-            // Update the record in database
-            const { error: updateError } = await supabase
-                .from('billing_cache')
-                .update({
-                    current_period_usage: {
-                        // @ts-expect-error The property exists
-                        ...billingRecord.current_period_usage,
-                    },
-                })
-                .eq('organization_id', organizationId)
-                .select();
+        //     // Update the record in database
+        //     const { error: updateError } = await supabase
+        //         .from('billing_cache')
+        //         .update({
+        //             current_period_usage: {
+        //                 // @ts-expect-error The property exists
+        //                 ...billingRecord.current_period_usage,
+        //             },
+        //         })
+        //         .eq('organization_id', organizationId)
+        //         .select();
 
-            if (updateError) throw updateError;
-        }
+        //     if (updateError) throw updateError;
+        // }
 
         return NextResponse.json(status);
     } catch (error) {
